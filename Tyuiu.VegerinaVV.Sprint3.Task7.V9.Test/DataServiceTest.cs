04afc8241bd78615e118119f94c2f5e0819d2b3c
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-10-26 22:38:51 +0500

    Task7

---
+++ Tyuiu.VegerinaVV.Sprint3.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task7.V9.Lib;
+
+namespace Tyuiu.VegerinaVV.Sprint3.Task7.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -27.22;
+            valueWaitArray[1] = -22.25;
+            valueWaitArray[2] = -16.66;
+            valueWaitArray[3] = -11.04;
+            valueWaitArray[4] = -6.13;
+            valueWaitArray[5] = -3.00;
+            valueWaitArray[6] = 4.84;
+            valueWaitArray[7] = 8.86;
+            valueWaitArray[8] = 14.43;
+            valueWaitArray[9] = 20.18;
+            valueWaitArray[10] = 25.24;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint3.Task7.V9/Program.cs
@@ -0,0 +1,60 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task7.V9.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+        Console.WriteLine("*        (2*x - 3)                                                        *");
+        Console.WriteLine("* F(x)= ------------ +5*x-sin(x) (произвести табулирование) f(x) на       *");
+        Console.WriteLine("*       cos(x) - 2*x                                                      *");
+        Console.WriteLine("* заданном диапазоне [-5;5] с шагом 1.Произвести проверку деления на ноль.*");
+        Console.WriteLine("* При делении на ноль вернуть значение 0. Значения округлить до двух      *");
+        Console.WriteLine("* знаков после запятой.                                                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+        double[] valueArray;
+        valueArray = new double[len];
+
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|     X    +   f(x)   +");
+        Console.WriteLine("+----------+----------+");
+
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
+        Console.ReadKey();
+    }
+}
