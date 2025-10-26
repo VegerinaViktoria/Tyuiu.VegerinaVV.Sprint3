9d9082b9e7198486740dbfae66cfe23667cadd86
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-10-26 14:48:34 +0500

    Task2

---
+++ Tyuiu.VegerinaVV.Sprint3.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task2.V3.Lib;
+
+namespace Tyuiu.VegerinaVV.Sprint3.Task2.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int value = 4;
+            int startValue = 1;
+            int stopValue = 15;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = 1935;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint3.Task2.V3/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task2.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет сумму   *");
+        Console.WriteLine("* ряда по формуле, при x=4                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int value = 4;
+        int startValue = 1;
+        int stopValue = 15;
+
+        Console.WriteLine("Переменная X = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+        Console.ReadKey();
+    }
+}
