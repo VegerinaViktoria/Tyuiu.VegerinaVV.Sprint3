49d7e13d5b4851260d617cfd98a66349207f3f4c
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-10-26 15:53:57 +0500

    Task3

---
+++ Tyuiu.VegerinaVV.Sprint3.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task3.V6.Lib;
+
+namespace Tyuiu.VegerinaVV.Sprint3.Task3.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceNumOnChar()
+        {
+            DataService ds = new DataService();
+
+            string str = "2as4e 5his 1ea";
+            char item = 't';
+
+            string res = ds.ReplaceNumOnChar(str, item);
+
+            string wait = "taste this tea";
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint3.Task3.V6/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task3.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach заменить цифры на букву t в строке:              *");
+        Console.WriteLine("* 12able 1o s4gh                                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string value = "12able 1o s4gh";
+        char item = 't';
+
+        Console.WriteLine("Исходная строка = " + value);
+        Console.WriteLine("Искомый символ = " + item);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Преобразованная строка = " + ds.ReplaceNumOnChar(value,item));
+
+        Console.ReadKey();
+    }
+}
