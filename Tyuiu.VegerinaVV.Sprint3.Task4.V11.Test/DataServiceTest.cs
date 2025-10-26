839d296548c315bb86a1c0b4da185a56ab02e65e
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-10-26 16:50:42 +0500

    Task4

---
+++ Tyuiu.VegerinaVV.Sprint3.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task4.V11.Lib;
+
+namespace Tyuiu.VegerinaVV.Sprint3.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 0.526;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint3.Task4.V11/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.VegerinaVV.Sprint3.Task4.V11.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=x/(sin(x) - x)+2. При х = 0 пропустить значение.              *");
+        Console.WriteLine("* Полученные значения перемножать.                                        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+
+        Console.ReadKey();
+    }
+}
